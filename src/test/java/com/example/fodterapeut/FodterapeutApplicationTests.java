6b4f60aa90f05b272d9ae104ccfca0ea926005bd
Christoffer Lund <Christofferlund93@gmail.com>
2025-05-05 14:44:09 +0200

    Initial commit

---
+++ src/test/java/com/example/fodterapeut/FodterapeutApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fodterapeut;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FodterapeutApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
