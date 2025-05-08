0dae3e7398e24a79ffe784ad2c80b44b17d5db16
Celine Lund Månsson <166127204+Celinelundm@users.noreply.github.com>
2025-05-08 15:47:26 +0200

    Lavet fragments filer i stedet for at kopiere det hele ind i hver html fil

---
+++ src/main/resources/static/js/layoutLoader.js
@@ -0,0 +1,12 @@
+function loadFragment(id, file) {
+  fetch(file)
+    .then(res => res.text())
+    .then(html => document.getElementById(id).innerHTML = html)
+    .catch(err => console.error(`Fejl ved indlæsning af ${file}:`, err));
+}
+
+window.addEventListener("DOMContentLoaded", () => {
+  loadFragment("topbar", "/fragments/topbar.html");
+  loadFragment("navbar", "/fragments/navbar.html");
+  loadFragment("footer", "/fragments/footer.html");
+});
